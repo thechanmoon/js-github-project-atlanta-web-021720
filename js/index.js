03685eb986688dfbcead6c9f88a89575d5cb9b58
thechanmoon <thechanmoon@gmail.com>
2020-04-06 22:42:36 -0400

    wip getData completed

--- js/index.js
+++ js/index.js
@@ -1,97 +1,79 @@
 //https://api.github.com/search/users?q=octocat
 const BASE_URL = 'https://api.github.com'
+//let github_form = document.querySelector('#github-form');
 
-document.addEventListener('DOMContentLoaded', (event) => {
-    console.log('DOM fully loaded and parsed');
-
+document.addEventListener('DOMContentLoaded', init);
+                           
+function init(event)
+{
     const github_form = document.querySelector('#github-form');
-    const github_container = document.querySelector('#github-container');
+    // const github_container = document.querySelector('#github-container');
     
-    github_form.addEventListener('submit',(event)=>{
+    github_form.addEventListener('submit',getGithubData)
     
-    event.preventDefault()
+}
 
+function getData(url)
+{
+    return fetch(url)
+}
+
+function getGithubData(event)
+{
     let user = event.target.search.value
-    let fetch_url = BASE_URL+"/search/users?q="+user
-    //debugger
-        // method: "POST" is missing from the object below
-    // let formData = {
-    //     dogName: "Byron",
-    //     dogBreed: "Poodle"
-    //     };    
+    // let fetch_url = BASE_URL+"/search/users?q="+user
 
-    // let configObj = {
-    //     headers: {
-    //     "Content-Type": "application/json",
-    //     // "Accept": "application/json"
-    //     "Accept": "application/vnd.github.v3+json"
-    //     },
-    //     // body: JSON.stringify(formData)
-    // };
-    //debugger 
-    fetch(fetch_url)
-        .then(function(response) {
-        return response.json();
-        })
-        .then(function(object) {
-        // console.log(object.items);
-        
-        //debugger
-        renderGithub(object);
-        })
-        .catch(function(error) {
-        alert("Bad things! Ragnarők!");
-        console.log(error.message);
-        });
-        })
+    event.preventDefault()
+    getData(BASE_URL+'/search/users?q='+user)
+    .then(res=>res.json())
+    .then(data=>{
+        parseData(data.items)
+    });
+}
 
-        function renderGithub(object)
-        {
-            let div = document.createElement('div') 
-            let h1 = document.createElement('h1')
-            let ul =  document.createElement('u1')
-            let li_1 =  document.createElement('li')
-            let li_2 =  document.createElement('li')
-            let li_3 =  document.createElement('li')
-            let li_4 =  document.createElement('li')
-            let li_5 =  document.createElement('li')
-            let li_6 =  document.createElement('li')
-            let li_7 =  document.createElement('li')
-            // div.appendChild(h1)
+function parseData(array)
+{
+    array.forEach(element => {
+        parseElement(element)
+    });
+}
 
-            object.items.forEach(item => {
-                
-                h1.innerText = item.login
-                div.appendChild(h1)                
- 
-                li_1.innerText = item.id
-                ul.appendChild(li_1)
-                li_2.innerText = item.node_id
-                ul.appendChild(li_2)
-                li_3.innerText = item.avatar_url
-                ul.appendChild(li_3)
-                li_4.innerText = item.url
-                ul.appendChild(li_4)
-                li_5.innerText = item.html_url
-                ul.appendChild(li_5)
+function parseElement(element)
+{
+    let github_container = document.querySelector('#github-container');
 
-                div.appendChild(ul)
-
-                github_container.appendChild(div)
-                //github_container.appendChild(item.login)
-            });
-        }
-});
+    let div = document.createElement('div') 
+    let h1 = document.createElement('h1')
+    let ul =  document.createElement('u1')
+    let li_1 =  document.createElement('li')
+    let li_2 =  document.createElement('li')
+    let li_3 =  document.createElement('li')
+    let li_4 =  document.createElement('li')
+    let li_5 =  document.createElement('li')
+    let li_6 =  document.createElement('li')
+    let li_7 =  document.createElement('li')
+    // div.appendChild(h1)
 
 
-// addAnother = function() {
-//     var ul = document.getElementById("list");
-//     var li = document.createElement("li");
-//     var children = ul.children.length + 1
-//     li.setAttribute("id", "element"+children)
-//     li.appendChild(document.createTextNode("Element "+children));
-//     ul.appendChild(li)
-// }
+    h1.innerText = element.login
+    div.appendChild(h1)                
+
+    li_1.innerText = element.id
+    ul.appendChild(li_1)
+    li_2.innerText = element.node_id
+    ul.appendChild(li_2)
+    li_3.innerText = element.avatar_url
+    ul.appendChild(li_3)
+    li_4.innerText = element.url
+    ul.appendChild(li_4)
+    li_5.innerText = element.html_url
+    ul.appendChild(li_5)
+
+    div.appendChild(ul)
+
+    github_container.appendChild(div)
+}
+
 
 /* login: "thechanmoon"
 id: 40532728
